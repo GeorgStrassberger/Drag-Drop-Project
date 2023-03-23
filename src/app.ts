96611a1aa6a07dd302c90b87d498bcfc25a3a477
Georg <georg.strassberger@googlemail.com>
2023-03-23 10:33:56 +0100

    Using a Getter to change between plural & singular textContent

--- src/app.ts
+++ src/app.ts
@@ -160,10 +160,16 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem Class
-
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
 
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
 
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
@@ -172,13 +178,11 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure(): void {
-
-    }
+    configure(): void { }
 
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('P')!.textContent = this.project.description;
     }
 }
