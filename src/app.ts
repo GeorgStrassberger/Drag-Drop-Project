1fd75b3bb86908d07d1b072aa85109a836df6981
Georg <georg.strassberger@googlemail.com>
2023-03-23 10:24:36 +0100

    rendering RojectItems with a Class

--- src/app.ts
+++ src/app.ts
@@ -159,6 +159,30 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// ProjectItem Class
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+
+    configure(): void {
+
+    }
+
+    renderContent(): void {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('P')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
@@ -192,15 +216,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
             this.type.toUpperCase() + " PROJECTS";
     }
 
-    private renderProjects() {
+    private renderProjects(): void {
         const listEl = document.getElementById(
             `${this.type}-projects-list`
         )! as HTMLUListElement;
         listEl.innerHTML = "";
         for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement("li");
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
     }
 }
